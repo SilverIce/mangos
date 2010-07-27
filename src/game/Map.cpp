0bee4bb5057c0ca50b3027c6cbc29a837b8d551c
SilverIce <slifeleaf@gmail.com>
2010-07-27 11:06:44 +0300

    simplify Map::CreatureRelocation logic

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -785,39 +785,26 @@ Map::CreatureRelocation(Creature *creature, float x, float y, float z, float ang
     CellPair new_val = MaNGOS::ComputeCellPair(x, y);
     Cell new_cell(new_val);
 
-    // delay creature move for grid/cell to grid/cell moves
     if (old_cell.DiffCell(new_cell) || old_cell.DiffGrid(new_cell))
     {
         DEBUG_FILTER_LOG(LOG_FILTER_CREATURE_MOVES, "Creature (GUID: %u Entry: %u) added to moving list from grid[%u,%u]cell[%u,%u] to grid[%u,%u]cell[%u,%u].", creature->GetGUIDLow(), creature->GetEntry(), old_cell.GridX(), old_cell.GridY(), old_cell.CellX(), old_cell.CellY(), new_cell.GridX(), new_cell.GridY(), new_cell.CellX(), new_cell.CellY());
 
-        // do move or do move to respawn or remove creature if previous all fail
-        if(CreatureCellRelocation(creature,new_cell))
+        // try to move to the new cell or move it to respawn -- do nothing if both operations failed
+        if (!CreatureCellRelocation(creature,new_cell) && !CreatureRespawnRelocation(creature))
         {
-            // update pos
-            creature->Relocate(x, y, z, ang);
+            DEBUG_FILTER_LOG(LOG_FILTER_CREATURE_MOVES, "Creature (GUID: %u Entry: %u ) can't be move to unloaded respawn grid.",creature->GetGUIDLow(),creature->GetEntry());
+            return;
+        }
+    }
 
-            // in diffcell/diffgrid case notifiers called in Creature::Update
-            creature->SetNeedNotify();
-        }
-        else
-        {
-            // if creature can't be move in new cell/grid (not loaded) move it to repawn cell/grid
-            // creature coordinates will be updated and notifiers send
-            if(!CreatureRespawnRelocation(creature))
-            {
-                // ... or unload (if respawn grid also not loaded)
-                DEBUG_FILTER_LOG(LOG_FILTER_CREATURE_MOVES, "Creature (GUID: %u Entry: %u ) can't be move to unloaded respawn grid.",creature->GetGUIDLow(),creature->GetEntry());
-                creature->SetNeedNotify();
-            }
-        }
-    }
-    else
-    {
-        creature->Relocate(x, y, z, ang);
-        creature->SetNeedNotify();
-    }
+    creature->Relocate(x, y, z, ang);
 
     creature->GetViewPoint().Call_UpdateVisibilityForOwner();
+    // do not use new_val, new_cell parameters here: they couldn't be actual after CreatureRespawnRelocation call
+    UpdateObjectVisibility(creature,creature->GetCurrentCell(),creature->GetCurrentCell().cellPair());
+    // notifiers called in Creature::Update
+    creature->SetNeedNotify();
+
     ASSERT(CheckGridIntegrity(creature,true));
 }
 
