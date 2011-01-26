b7e3910fb4b34947c6007b8d0ff1fdf980be5baf
SilverIce <slifeleaf@gmail.com>
2011-01-26 02:39:29 +0200

    Ace pch

---
+++ dep/ACE_wrappers/ace_pch.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include "ace/ACE.h"
+//#include "ac/RW_Thread_Mutex.h"
+
+#include "ace/Basic_Types.h"
+#include "ace/Synch_Traits.h"
+#include "ace/Svc_Handler.h"
+#include "ace/SOCK_Stream.h"
+#include "ace/SOCK_Acceptor.h"
+#include "ace/Acceptor.h"
+#include "ace/Thread_Mutex.h"
+#include "ace/Guard_T.h"
+#include "ace/Unbounded_Queue.h"
+#include "ace/Message_Block.h"
+
